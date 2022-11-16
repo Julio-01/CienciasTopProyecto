20b345d58a9a41990f961201f1bd9647535450ab
Valeria Manjarrez Angeles <valeriamanjreez@MacBook-Air-de-Valeria.local>
2022-11-15 20:01:45 -0600

    Final version

---
+++ cienciasTop/src/main/java/com/cienciasTop/models/service/RoleServiceImpl.java
@@ -0,0 +1,35 @@
+package com.cienciasTop.models.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cienciasTop.models.dao.IRoleDao;
+import com.cienciasTop.models.entity.Role;
+
+@Service
+public class RoleServiceImpl implements IRoleService {
+
+	@Autowired
+	private IRoleDao roleDao;
+
+	@Override
+	@Transactional(readOnly=true)
+	public List<Role> findAll() {
+		return (List<Role>) roleDao.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly=true)
+	public Role findById(Long id) {
+		return roleDao.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional(readOnly=true)
+	public Role findByNombre(String nombre) {
+		return roleDao.findByNombre(nombre);
+	}
+}
